732c459940051d762f4276f129e540e8be0e4bcd
Alexander <alexander-rebai@hotmail.com>
2023-08-09 11:19:19 +0200

    Revert "clear"
    This reverts commit 41c6fdd671565857725b48d9501a0f09c06d7080.

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,16 +1,10 @@
-import Navbar from "@/components/navbar";
-import Sidebar from "@/components/sidebar";
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden hß-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900 text-white">
-        <Sidebar />
+        <div>Hello Sidebar</div>
       </div>
-      <main className="md:pl-72">
-        <Navbar />
-        {children}
-      </main>
+      <main className="md:pl-72">{children}</main>
     </div>
   );
 };
