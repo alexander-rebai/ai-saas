f452999eb9e2c2905c76faeecdee3b490559fc82
Alexander <alexander-rebai@hotmail.com>
2023-08-22 16:25:20 +0200

    fix error in console for nesting button in shadCn sheetTrigger

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -21,9 +21,9 @@ const MobileSidebar = () => {
   return (
     <Sheet>
       <SheetTrigger>
-        <Button variant="ghost" size="icon" className="md:hidden">
+        <div className="h-10 w-10 md:hidden hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
           <Menu />
-        </Button>
+        </div>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <Sidebar />
