e5ed291b2609ae4d4e358c907c9ea1518b18c397
Alexander <alexander-rebai@hotmail.com>
2023-08-26 15:18:44 +0200

    fix typo

--- app/(platform)/(routes)/code/page.tsx
+++ app/(platform)/(routes)/code/page.tsx
@@ -130,7 +130,7 @@ const CodePage = () => {
             </div>
           )}
           {messages.length === 0 && !isLoading && (
-            <Empty label="No conversation started" />
+            <Empty label="No code generated" />
           )}
           <div className="flex flex-col gap-y-4">
             {messages.map((message) => (
