94660d40c8ca06215be7c0bc84d33e4f416efebe
alexander-rebai <140480698+alexander-rebai@users.noreply.github.com>
2023-10-01 12:58:31 +0200

    create testimonials component on landing page (#4)

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import Testimonials from "@/components/testimonials";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
+      <Testimonials />
     </div>
   );
 };
